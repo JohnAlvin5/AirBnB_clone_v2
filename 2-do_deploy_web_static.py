55fb999f21fda354196094d3f6efa1377a474b8e
Alvin Wanangwe <WanangweAlvin@gmail.com>
2024-02-13 15:11:43 +0300

    Added files on 0x03. AirBnB clone - Deploy Static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# Distributes an archive to a web server
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+import os.path
+
+env.hosts = ["100.25.117.101", "54.160.106.191"]
+
+
+def do_deploy(archive_path):
+    """ Distributes an archive to my web server
+        Otherwise returns false
+    """
+    if os.path.isfile(archive_path) is False:
+        return False
+    
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
